9c4aa65f31b6e879c8a491413ea2a73fb50a7355
Tom Baranes <tom.baranes@epitech.eu>
2014-05-14 11:35:37 +0900

    Add licence’s header in every files.

--- FSI/UIImage+FSIExtensions.m
+++ FSI/UIImage+FSIExtensions.m
@@ -1,10 +1,19 @@
 //
-//  UIImage+FSI.m
-//  FriendlyQuiz
+// UIImage+FSI.m
 //
-//  Created by Tom Baranes on 04/11/13.
-//  Copyright (c) 2013 Tom Baranes. All rights reserved.
+// Copyright (c) 2013 Tom Baranes
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #import "UIImage+FSIExtensions.h"
 
--- FSI/UIScrollView+FSIExtensions.h
+++ FSI/UIScrollView+FSIExtensions.h
@@ -1,10 +1,19 @@
 //
-//  UIScrollView+FSIExtensions.h
-//  Jukeboard
+// UIScrollView+FSIExtensions.h
 //
-//  Created by Tom Baranes on 7/24/13.
-//  Copyright (c) 2013 Tom Baranes. All rights reserved.
+// Copyright (c) 2013 Tom Baranes
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #import <UIKit/UIKit.h>
 
